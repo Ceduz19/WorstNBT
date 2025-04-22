80fab96e671a7d1d161b06ec5cebcd8aae7e17d8
Ceduz19 <edoce01@gmail.com>
2025-04-22 21:03:38 +0200

    new useful methods for NBTList

--- core/src/main/java/me/ceduz19/worstnbt/core/NBTList.java
+++ core/src/main/java/me/ceduz19/worstnbt/core/NBTList.java
@@ -2,6 +2,7 @@ package me.ceduz19.worstnbt.core;
 
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.NonExtendable
 public interface NBTList extends NBTCollection<NBT> {
@@ -10,26 +11,34 @@ public interface NBTList extends NBTCollection<NBT> {
         return WorstNBT.list();
     }
 
-    @NotNull NBTCompound getCompound(int var1);
+    @NotNull NBTCompound getCompound(int index);
 
-    @NotNull NBTList getList(int var1);
+    @NotNull NBTList getList(int index);
 
-    short getShort(int var1);
+    short getShort(int index);
 
-    int getInt(int var1);
+    int getInt(int index);
 
-    int[] getIntArray(int var1);
+    int[] getIntArray(int index);
 
-    long[] getLongArray(int var1);
+    long[] getLongArray(int index);
 
-    double getDouble(int var1);
+    double getDouble(int index);
 
-    float getFloat(int var1);
+    float getFloat(int index);
 
-    @NotNull String getString(int var1);
+    @NotNull String getString(int index);
 
     @Override
     default @NotNull NBTType getType() {
         return NBTType.LIST;
+    }
+
+    default @Nullable NBTCompound getCompoundOrNull(int index) {
+        return getElementType() == NBTType.COMPOUND ? getCompound(index) : null;
+    }
+
+    default @Nullable NBTList getListOrNull(int index) {
+        return getElementType() == NBTType.LIST ? getList(index) : null;
     }
 }
