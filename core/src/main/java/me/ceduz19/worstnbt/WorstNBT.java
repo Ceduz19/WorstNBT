03d8b5aaafb80d2d6094b35f5bf29a3285219587
Ceduz19 <edoce01@gmail.com>
2025-11-20 20:07:08 +0100

    try fix jitpack.yml

--- core/src/main/java/me/ceduz19/worstnbt/WorstNBT.java
+++ core/src/main/java/me/ceduz19/worstnbt/WorstNBT.java
@@ -40,92 +40,79 @@ public final class WorstNBT {
     );
     private static final boolean SUPPORTED = COMPATIBILITY.isSupported();
     private static final NBTInternal INTERNAL;
+    private static final Class<? extends BlockState>[] TILE_ENTITIES;
 
-    @NotNull
-    public static NBTByteArray byteArray(byte[] a) {
+    public static @NotNull NBTByteArray byteArray(byte[] array) {
         checkCompatibility();
-        return INTERNAL.byteArray(a);
+        return INTERNAL.byteArray(array);
     }
 
-    @NotNull
-    public static NBTCompound compound() {
+    public static @NotNull NBTCompound compound() {
         checkCompatibility();
         return INTERNAL.compound();
     }
 
-    @NotNull
-    public static NBTEnd end() {
+    public static @NotNull NBTEnd end() {
         checkCompatibility();
         return INTERNAL.end();
     }
 
-    @NotNull
-    public static NBTIntArray intArray(int[] a) {
+    public static @NotNull NBTIntArray intArray(int[] array) {
         checkCompatibility();
-        return INTERNAL.intArray(a);
+        return INTERNAL.intArray(array);
     }
 
-    @NotNull
-    public static NBTList list() {
+    public static @NotNull NBTList list() {
         checkCompatibility();
         return INTERNAL.list();
     }
 
-    @NotNull
-    public static NBTLongArray longArray(long[] a) {
+    public static @NotNull NBTLongArray longArray(long[] array) {
         checkCompatibility();
         if (NMSVer.SERVER.isBefore(NMSVer.V1_12_R1))
             throw new UnsupportedOperationException("Long array NBT is not implemented in this minecraft version (" +
                     NMSVer.MC_SERVER_VER + "), must be at least 1.12");
 
-        return INTERNAL.longArray(a);
+        return INTERNAL.longArray(array);
     }
 
-    @NotNull
-    public static NBTNumeric.Byte byteNum(byte b) {
+    public static @NotNull NBTNumeric.Byte byteNum(byte b) {
         checkCompatibility();
         return INTERNAL.byteNum(b);
     }
 
-    @NotNull
-    public static NBTNumeric.Short shortNum(short s) {
+    public static @NotNull NBTNumeric.Short shortNum(short s) {
         checkCompatibility();
         return INTERNAL.shortNum(s);
     }
 
-    @NotNull
-    public static NBTNumeric.Int intNum(int i) {
+    public static @NotNull NBTNumeric.Int intNum(int i) {
         checkCompatibility();
         return INTERNAL.intNum(i);
     }
 
-    @NotNull
-    public static NBTNumeric.Long longNum(long l) {
+    public static @NotNull NBTNumeric.Long longNum(long l) {
         checkCompatibility();
         return INTERNAL.longNum(l);
     }
 
-    @NotNull
-    public static NBTNumeric.Float floatNum(float f) {
+    public static @NotNull NBTNumeric.Float floatNum(float f) {
         checkCompatibility();
         return INTERNAL.floatNum(f);
     }
 
-    @NotNull
-    public static NBTNumeric.Double doubleNum(double d) {
+    public static @NotNull NBTNumeric.Double doubleNum(double d) {
         checkCompatibility();
         return INTERNAL.doubleNum(d);
     }
 
-    @NotNull
-    public static NBTString string(@NotNull String string) {
+    public static @NotNull NBTString string(@NotNull String string) {
         checkCompatibility();
         Objects.requireNonNull(string, "string");
         return INTERNAL.string(string);
     }
 
-    @NotNull
-    public static NBTCompound fromItemStack(@NotNull ItemStack itemStack) {
+    public static @NotNull NBTCompound fromItemStack(@NotNull ItemStack itemStack) {
         checkCompatibility();
         Objects.requireNonNull(itemStack, "itemStack");
         if (itemStack.getType() == Material.AIR || itemStack.getAmount() <= 0)
@@ -133,21 +120,7 @@ public final class WorstNBT {
         return INTERNAL.fromItemStack(itemStack);
     }
 
-    private static final Class<? extends BlockState>[] TILE_ENTITIES;
-
-    private static @Nullable RuntimeException isTileEntity(@NotNull BlockState blockState) {
-        if (NMSVer.SERVER.isAtLeast(NMSVer.V1_14_R1))
-            return blockState instanceof TileState ? null : new IllegalArgumentException("block state must be an instance of " + TileState.class.getCanonicalName());
-
-        if (Stream.of(TILE_ENTITIES).anyMatch(c -> c.isInstance(blockState)) ||
-                blockState.getType() == Material.PISTON || blockState.getType() == Material.END_PORTAL)
-            return null;
-
-        return new IllegalArgumentException("block state must be one of the following blocks: ");
-    }
-
-    @NotNull
-    public static NBTCompound fromBlock(@NotNull BlockState block) {
+    public static @NotNull NBTCompound fromBlock(@NotNull BlockState block) {
         checkCompatibility();
         Objects.requireNonNull(block, "block");
 
@@ -157,28 +130,25 @@ public final class WorstNBT {
         return INTERNAL.fromBlock(block);
     }
 
-    @NotNull
-    public static NBTCompound fromEntity(@NotNull Entity entity) {
+    public static @NotNull NBTCompound fromEntity(@NotNull Entity entity) {
         checkCompatibility();
         Objects.requireNonNull(entity, "entity");
         return INTERNAL.fromEntity(entity);
     }
 
-    public static NBTCompound fromScoreboard(@NotNull Scoreboard scoreboard) {
+    public static @NotNull NBTCompound fromScoreboard(@NotNull Scoreboard scoreboard) {
         checkCompatibility();
         Objects.requireNonNull(scoreboard, "scoreboard");
         return INTERNAL.fromScoreboard(scoreboard);
     }
 
-    @NotNull
-    public static NBTCompound fromFile(@NotNull File file) throws IOException {
+    public static @NotNull NBTCompound fromFile(@NotNull File file) throws IOException {
         checkCompatibility();
         Objects.requireNonNull(file, "file");
         return INTERNAL.fromFile(file);
     }
 
-    @NotNull
-    public static NBTCompound fromInputStream(@NotNull InputStream inputStream) throws IOException {
+    public static @NotNull NBTCompound fromInputStream(@NotNull InputStream inputStream) throws IOException {
         checkCompatibility();
         Objects.requireNonNull(inputStream, "inputStream");
         return INTERNAL.fromInputStream(inputStream);
@@ -193,6 +163,17 @@ public final class WorstNBT {
         if (INTERNAL == null)
             throw new IllegalStateException("Unable to create an instance of the internal class (package: " +
                     WorstNBT.class.getPackage().getName() + "." + NMSVer.SERVER + ".WorstNBTInternal), cannot find constructor?");
+    }
+
+    private static @Nullable RuntimeException isTileEntity(@NotNull BlockState blockState) {
+        if (NMSVer.SERVER.isAtLeast(NMSVer.V1_14_R1))
+            return blockState instanceof TileState ? null : new IllegalArgumentException("block state must be an instance of " + TileState.class.getCanonicalName());
+
+        if (Stream.of(TILE_ENTITIES).anyMatch(c -> c.isInstance(blockState)) ||
+                blockState.getType() == Material.PISTON || blockState.getType() == Material.END_PORTAL)
+            return null;
+
+        return new IllegalArgumentException("block state must be one of the following blocks: ");
     }
 
     static {
